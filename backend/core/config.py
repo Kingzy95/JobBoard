b18a4fd613a99cbb6d684bbe44bdef1e6e6ce7d3
Emmanuel Mingui <emmanuelmingui@gmail.com>
2022-10-14 15:29:10 +0200

    CREATE DATABASE AND MODELS (ORM)

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 class Settings:
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
+
+    MYSQL_USER : str = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_SERVER : str = os.getenv("MYSQL_SERVER","127.0.0.1")
+    MYSQL_PORT : str = os.getenv("MYSQL_PORT",8889)
+    MYSQL_DB : str = os.getenv("MYSQL_DB","joby")
+    DATABASE_URL = f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DB}"
 
 settings = Settings()
